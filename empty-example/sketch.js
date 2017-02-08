0439e2f0ae4449ab39a8d41c756959e17a846c99
Keith O'Hara <kohara@bard.edu>
2017-02-08 16:16:11 -0500

    static sketch

---
+++ empty-example/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+    createCanvas(400, 400);
+    background(24);
+
+    // note to self: eyes
+    // and nose
+    fill(255, 0, 0);
+    ellipse(200, 200, 50, 25);
+    rect(140, 150, 50, 20);
+    rect(210, 150, 50, 20);
+
+    // note to self: head
+    noFill();
+    stroke(255);
+    strokeWeight(4);
+    ellipse(200, 200,180,180);
+
+}
+
+function draw() {
+
+}
