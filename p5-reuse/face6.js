397b10bd4f1637ef4e929e581459dc7c97893f61
Keith O'Hara <kohara@bard.edu>
2017-03-01 19:44:26 -0500

    reuse sketches

---
+++ p5-reuse/face6.js
@@ -0,0 +1,49 @@
+function setup() {
+    createCanvas(50 * 10, 80 * 8);
+    rectMode(CENTER);
+    frameRate(2);
+}
+
+function draw() {
+    background(128);
+    var y = 40;
+    while (y < height){
+        var x = 25;
+        while (x < width) {
+            face(x, y);
+            x = x + 50;
+        }
+        y = y + 80;
+    }
+}
+
+function face(x, y) {
+    push();
+    translate(x, y);
+    fill(random(160, 255));
+    stroke(64);
+    strokeWeight(2);
+    var sz = random(4, 12);
+    var ar = random(0.75, 1.25);
+    ellipse(0, 0, 45, 70);
+
+    fill(24);
+    noStroke();
+    ellipse(-8, -18, sz, sz * ar);
+    ellipse(8, -18, sz, sz * ar);
+
+    stroke(24);
+    strokeWeight(random(3));
+    var y1 = random(-30, -20);
+    line(-11, random(-30, -20), -4, y1);
+    line(4, y1, 11, random(-30, -20));
+
+    strokeWeight(random(2, 3));
+    var y1 = random(20);
+    beginShape();
+    vertex(-10, random(20));
+    vertex(0, y1);
+    vertex(10, random(20));
+    endShape(CLOSE);
+    pop();
+}
